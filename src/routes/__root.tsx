40beac40eb6590e392998e4dc11e9d87ac71da2f
Gustavo Bobock <bdk.arcos@gmail.com>
2026-07-10 15:56:13 -0300

    Tradução automatica estava atrapanlhando - feat: add 'notranslate' meta tag for improved SEO and accessibility

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -77,6 +77,7 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
     meta: [
       { charSet: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { name: "google", content: "notranslate" },
       { name: "author", content: "Bodock Planet Tecnologia" },
       { property: "og:type", content: "website" },
       { name: "twitter:card", content: "summary_large_image" },
